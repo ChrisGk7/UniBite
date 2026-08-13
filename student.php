fffe1ef8730461c7eaf654a41e2365f3c540d1d3
dimvr <dimvri112@gmail.com>
2026-08-13 17:39:15 +0300

    Added validation

--- student.php
+++ student.php
@@ -1,12 +1,34 @@
 <?php
+session_start();
 include("database.php");
-include("logout.php");
 
-
+// Require a logged-in session to view this page
+if (!isset($_SESSION['username'])) {
+    header("Location: index.php");
+    exit();
+    
+}
+$student = get_user_by_username($_SESSION['username'], $conn);
 ?>
 <html>
-
-<h1>Student Dashboard</h1>;
-
-<button onclick="window.location.href='logout.php'">Logout</button>
+<head>
+   
+    <link rel="stylesheet" href="studentd.css">
+</head>
+<body>
+<div class="logo-container">
+    <button class="logo" data-text="Awesome" onclick="window.location.href='logout.php'">
+        <span class="actual-text">&nbsp;UNiBITE&nbsp;</span>
+        <span aria-hidden="true" class="hover-text">&nbsp;UNiBITE&nbsp;</span>
+    </button>
+</div>
+        <div class="dashboard-container">
+            <h1>Student Dashboard</h1>
+            <p>Welcome, <?php echo htmlspecialchars($student['name']); ?>!</p>
+        </div>
+<button  class="btn" onclick="window.location.href='logout.php'">Logout</button>
+<button id="adddishBtn" class="btn" onclick="window.location.href='cook.html'">Add Dish </button>
+</body>
 </html>
+
+<script src="student.js" defer></script>
